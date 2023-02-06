61de5f0aa8bf86bd275c75df3c157c282238fb86
Yevhen Kushnir <yevhen.kushnir@gmail.com>
2023-02-06 10:12:05 +0200

    font awesome install

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { VueQueryPlugin } from 'vue-query';
 import './index.css';
 import 'mosha-vue-toastify/dist/style.css';
 
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
 import App from './App.vue';
 import router from './router';
 
